6fdc941ae1c84be4ae5b06b20f397989d65db8d8
Alex Panasko <a.panasko@bk.ru>
2023-07-17 23:38:10 +0500

    Initial commit

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,45 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataHelper {
+    private static Faker faker = new Faker(new Locale("en"));
+
+    private DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static String generateRandomLogin() {
+        return faker.name().username();
+    }
+    public static String generateRandomPassword() {
+        return faker.internet().password();
+    }
+    public static AuthInfo generateRandomUser() {
+        return new AuthInfo(generateRandomLogin(),generateRandomPassword());
+    }
+
+    public static AuthInfo getAuthInfo() {
+
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+
+}
